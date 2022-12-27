3454543ed67e536d5f2ba4ea8cc275e264f0ddf7
CropTrak-Corteva <sup.croptrak@gmail.com>
2022-12-27 18:34:18 -0300

    Subir codigo config

---
+++ 02-Scripts/Codigo-de-configuracao.R
@@ -0,0 +1,8 @@
+usethis::use_git_config(
+  user.name = "CropTrak-Corteva",
+  user.email = "sup.croptrak@gmail.com"
+)
+usethis::create_github_token()
+usethis::edit_r_environ()
+usethis::git_sitrep()
+# gitcreds::gitcreds_set()
